55873a45317f5a7db246c176049e9f1a516c1b67
Marcos <marcosbastidas301@gmail.com>
2021-08-09 12:27:41 -0400

    map_object

--- src/components/tareas/index.js
+++ src/components/tareas/index.js
@@ -1,22 +1,64 @@
+
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
 import {traerTareas} from '../store/action/tareasAction'
+import Snnipers from '../users/Snnipers'
 
-const Tareas = ({traerTareas}) => {
-
+const Tareas = ({traerTareas,
+                tareas,
+                cargando,
+                error
+                    }) => {
     useEffect(()=> {
         traerTareas()
     }, [traerTareas])
+    //const [ usuario, setUsuario] = usuState({})
 
+    const mostarContenido = () =>(
+        
+            cargando ? <Snnipers />
+            :
+            Object.keys(tareas).map((usu_id, index) =>(
+                <div key={usu_id}>
+                    <h2>Usuario {usu_id}</h2>
+                    <div className='contenedor_tareas'>
+                        {ponerTareas(usu_id)}
+                    </div>
+                </div>
+            ))
+    );
+    const ponerTareas =(usu_id) =>{
+        const porUsuario =  {...tareas[usu_id]}
+        return Object.keys(porUsuario).map(tar_id =>(
+            <div key={tar_id}>
+                <input type='checkbox' defaultChecked={porUsuario[tar_id].completed} />
+                {
+                    porUsuario[tar_id].title
+                }
+            </div>
+        ))
+    
+    }
 
     return (
         <div>
-            <h1>tareas saludar marcos</h1>
+            <button>
+                Agregar
+            </button>
+            {mostarContenido()}
         </div>
     )
 }
 
-const mapSateToProps= ({tareasReducer}) => ({tareasReducer})
+
+
+
+
+const mapSateToProps= ({tareasReducer}) => ({
+    tareas:tareasReducer.tareas,
+    cargando: tareasReducer.cargando,
+    error:tareasReducer.error
+})
 
 const mapDispatchToProps = (dispatch) => {
 
